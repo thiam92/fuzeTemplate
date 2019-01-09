f5cf48a6df60ad3efdf5fba8f62f843abf71db7d
genieouz <genieouzog@gmail.com>
2019-01-09 11:58:10 +0000

    migration vers angular 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import 'hammerjs';
+
+import { FuseModule } from '@fuse/fuse.module';
+import { FuseSharedModule } from '@fuse/shared.module';
+
+import { fuseConfig } from './fuse-config';
 
 import { AppComponent } from './app.component';
+import { FuseMainModule } from './main/main.module';
+import { FuseSampleModule } from './main/content/sample/sample.module';
+import { OtherComponent } from './other/other.component';
+
+const appRoutes: Routes = [
+    {
+        path      : '**',
+        redirectTo: 'sample',
+
+      
+    },   
+    
+    {
+        path      : 'other',
+        redirectTo: 'OtherComponent',
+
+      
+    }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        OtherComponent
+    ],
+    imports     : [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes),
+        TranslateModule.forRoot(),
+
+        // Fuse Main and Shared modules
+        FuseModule.forRoot(fuseConfig),
+        FuseSharedModule,
+        FuseMainModule,
+        FuseSampleModule
+    ],
+    bootstrap   : [
+        AppComponent
+    ]
 })
-export class AppModule { }
+export class AppModule
+{
+}
